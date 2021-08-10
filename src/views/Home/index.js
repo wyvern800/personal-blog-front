69f2c7d0c60ab21a3a205fec1b472fc3a05ff8b9
wyvern800 <matheusguiferreira@hotmail.com>
2021-08-10 20:03:57 -0300

    My changes

--- src/views/Home/index.js
+++ src/views/Home/index.js
@@ -2,10 +2,16 @@ import React from "react";
 
 import { Container } from './styles';
 
+// or less ideally
+import { Button } from 'react-bootstrap';
+
 export default function Home() {
   return (
     <>
       <Container>
+        <Button>
+          Xota
+        </Button>
       This is the basic application - Home/index.js
         <p>This is not a paragraph, it is an idea</p>
       </Container>
