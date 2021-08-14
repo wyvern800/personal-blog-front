b61de638b8970092ad8ab3e304bf4b1dff3a7be2
wyvern800 <matheusguiferreira@hotmail.com>
2021-08-14 10:38:07 -0300

    Added base Navbar

--- src/assets/components/Header/index.js
+++ src/assets/components/Header/index.js
@@ -1,13 +1,18 @@
 import React, { Component } from 'react';
-import { Container } from './styles';
+import { Container, NavigationBar } from './styles';
+import { Link } from "react-router-dom";
+import { Navbar, Nav } from "react-bootstrap";
 
 export default class Header extends Component {
   render() {
     return (
       <Container>
-        <nav>
-          <h1>Header pimposo</h1>
-        </nav>
+        <NavigationBar>
+          <ul>
+            <li><Link to="">Home</Link></li>
+            <li><Link to="/about">About</Link></li>
+          </ul>
+        </NavigationBar>
       </Container>
     );
   }
--- src/assets/components/Header/styles.js
+++ src/assets/components/Header/styles.js
@@ -5,3 +5,17 @@ export const Container = styled.div`
     color: red;
   }
 `;
+
+export const NavigationBar = styled.nav`
+  ul {
+    list-style-type: none;
+    margin: 0px;
+    padding: 0px;
+    display: flex;
+    justify-content: center;
+
+    li {
+      margin-left: 10px;
+    }
+  }
+`;
