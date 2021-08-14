1fb9f7787737cf34592da1ef3da89d03a9566be8
Victor Bruno Paro <victorbparo@gmail.com>
2021-08-14 10:06:35 -0300

    Created About page

--- src/routes.js
+++ src/routes.js
@@ -3,12 +3,14 @@ import { Switch, Route } from "react-router-dom";
 
 import Home from './views/Home';
 import Article from './views/Article';
+import About from './views/About';
 
 export default function Routes() {
   return (
     <Switch>
         <Route path="/" exact component={Home} />
-        <Route path="/articles" exact component={Article} />
+        <Route path="/articles" component={Article} />
+        <Route path="/about" component={About}/>
         {/**<Route path="/characters" exact component={Character} />**/}
     </Switch>
   );
