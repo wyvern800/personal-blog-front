148656bdc72899352556919176d23ead59f5411d
Victor Bruno Paro <victorbparo@gmail.com>
2021-08-19 18:41:03 -0300

    Started structuring dev about sections, filling mine with info

--- src/views/About/styles.js
+++ src/views/About/styles.js
@@ -21,7 +21,10 @@ export const Main = styled.div`
     }
 `
 export const AboutSection = styled.div`
+    display: flex;
+    flex-direction: row;
     margin-top: 2%;
     margin-bottom: 2%;
     border: 1px solid red;
+    color: black;
 `
