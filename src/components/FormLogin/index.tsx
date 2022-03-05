449bdb2f7730afc55bc9de525a34527f6e672f95
Victor Bruno Paro <victorbparo@gmail.com>
2022-03-05 16:57:07 -0300

    Changing api calls from mock to local backend

--- src/components/FormLogin/index.tsx
+++ src/components/FormLogin/index.tsx
@@ -10,6 +10,8 @@ import { yupResolver } from '@hookform/resolvers/yup';
 
 import { fakeAuth } from '../../services/fakeApi';
 
+import api from '../../services/api';
+
 import { useLocation, useHistory } from 'react-router-dom';
 
 const FormLogin = () => {
@@ -28,6 +30,7 @@ const FormLogin = () => {
     register,
     handleSubmit,
     formState: { errors },
+    setError
   } = useForm({ resolver: yupResolver(validationSchema) });
 
   const { state } = useLocation();
@@ -37,12 +40,22 @@ const FormLogin = () => {
    * Executado ao dar submit no forum de login
    * @param data Dados do login
    */
-  const onSubmit = (data: any) => {
+  const onSubmit = async (data: any) => {
     console.log(data);
 
-    fakeAuth.authenticate(() => {
+    await api.post('/login', { uid: data?.email, password: data?.password }).then((response) => {
+      //history.push(state?.from || '/');
+      console.log(response)
+    }).catch((e) => {
+      setError("email", {
+        type: "manual",
+        message: "Autenticação falhou, verifique se o login ou senha não contém erros"
+      })
+    })
+
+    /*fakeAuth.authenticate(() => {
       history.push(state?.from || '/');
-    });
+    });*/
 
   };
 
