7bb07ff4b301c153dc93023955b07cf7792a1182
wyvern800 <matheusguiferreira@hotmail.com>
2022-04-10 21:10:55 -0300

    Added loading to PostsList

--- src/pages/Logout/index.tsx
+++ src/pages/Logout/index.tsx
@@ -2,6 +2,10 @@ import React, { useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
 
 import { useUserObjectData } from '../../hooks/UserObjectData';
+import { Wrapper } from './styles';
+import auth from '../../services/authService';
+import Loading from '../../components/Loading';
+import PulseLoader from "react-spinners/PulseLoader";
 
 const Logout = () => {
   const { setUserObjectData } = useUserObjectData();
@@ -9,16 +13,21 @@ const Logout = () => {
 
   // Handles the logging out
   const handleLogout = () => {
-    // Removes the token
-    localStorage.removeItem('token');
+    // Logs out only if the user is logged in
+    if (auth.isUserLogged()) {
+      // Removes the token
+      localStorage.removeItem('token');
 
-    // Clears data from the context
-    setUserObjectData(null);
+      // Clears data from the context
+      setUserObjectData(null);
 
-    // Redirect the logged out user to login after 3 seconds
-    setTimeout(() => {
-      history.push('/login');
-    }, 3000);
+      // Redirect the logged out user to login after 3 seconds
+      setTimeout(() => {
+        history.push('/login');
+      }, 2000);
+    } else {
+      history.push('/');
+    }
   };
 
   // Effect that triggers the logout
@@ -26,7 +35,11 @@ const Logout = () => {
     handleLogout();
   }, []);
 
-  return <>Você foi deslogado...</>;
+  return (
+    <Wrapper>
+      <Loading Icon={PulseLoader} text={'You logged out, redirecting you to homepage'} size={30} />
+    </Wrapper>
+  );
 };
 
 export default Logout;
